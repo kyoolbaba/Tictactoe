d7b63c780ccbff06be46d85f79d299774868f50b
kyoolbaba <kyoolbaba@gmail.com>
2020-04-17 21:08:17 +0530

    Refactored (alignment)

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 chooseXorO(){ 
+#ASSIGNING X OR O FEATURE
     declare toss=("Heads" "Tails")
- if [[ $((RANDOM%2)) -eq '0'  ]]; then
-    declare -a game=(X O)
-else
- declare -a game=(O X)
-fi
-echo letter assigned to you is ${game[0]} the computer is ${game[1]}
-
-echo Enter 0 for heads and 1 for tails
-read TOSSED
-r=${toss[$((RANDOM%2))]} 
-if [[ "${toss[TOSSED]}" == "$r" ]]; then
-   echo Congrats!! you Won the Toss it is $r  
+    if [[ $((RANDOM%2)) -eq '0'  ]]; then
+        declare -a game=(X O)
+    else
+        declare -a game=(O X)
+    fi
+    echo letter assigned to you is ${game[0]} the computer is ${game[1]}
+#TOSS FEATURE
+    echo Enter 0 for heads and 1 for tails
+    read TOSSED
+    r=${toss[$((RANDOM%2))]} 
+    if [[ "${toss[TOSSED]}" == "$r" ]]; then
+        echo Congrats!! you Won the Toss it is $r  
    else
-   echo Sorry!! you  Lost the Toss it is ${toss[((TOSSED+1)%2)]}
-fi
+        echo Sorry!! you  Lost the Toss it is ${toss[((TOSSED+1)%2)]}
+    fi
 }
 
 resetBox(){
