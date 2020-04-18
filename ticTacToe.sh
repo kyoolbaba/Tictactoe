dee54d84f2d444e7a3a62c05f7dc76710af7aca1
kyoolbaba <kyoolbaba@gmail.com>
2020-04-18 13:43:11 +0530

    Added feature to Enter the X/O in the board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+declare -a game
+declare place=0
 
-chooseXorO(){ 
+assignXorO(){ 
 #ASSIGNING X OR O FEATURE
     declare toss=("Heads" "Tails")
     if [[ $((RANDOM%2)) -eq '0'  ]]; then
-        declare -a game=(X O)
+        game=("| X |" "| O |")
     else
-        declare -a game=(O X)
+        game=("| O |" "| X |")
     fi
     echo letter assigned to you is ${game[0]} the computer is ${game[1]}
 #TOSS FEATURE
@@ -15,21 +17,43 @@ chooseXorO(){
     r=${toss[$((RANDOM%2))]} 
     if [[ "${toss[TOSSED]}" == "$r" ]]; then
         echo Congrats!! you Won the Toss it is $r  
+        count=0
    else
         echo Sorry!! you  Lost the Toss it is ${toss[((TOSSED+1)%2)]}
+        count=1
     fi
 }
 
-resetBox(){
-declare -a int box
+startgame(){
+    for (( k = 0 ; k < 9 ; k++  )); do
+    echo Enter the cell that you want to enter the ${game[count]}
+    read place
+    Box $place
+    count=$(((count+1)%2))
+    done
+}
+Box(){
+task=$1
+cell=1
 for (( i = 0 ; i < 3 ; i++  )); do
-for (( j = 0 ; j < 3 ; j++ )); do
-box[$i,$j]=0
-echo -n " " ${box[i,j]}" "
-done
-echo
-echo
+    for (( j = 0 ; j < 3 ; j++ )); do
+        if [[ "$task" == "reset" ]]; then 
+            box[$cell]="|___|"
+            echo -n " " ${box[$cell]}" "
+        else
+            if [[ $cell -eq $task ]]; then
+                box[$cell]=${game[count]}
+                echo -n " " ${box[$cell]}" " 
+            else 
+                 echo -n " " ${box[$cell]}" " 
+             fi   
+        fi
+        ((cell++))    
+    done
+    echo;echo
 done
 }
-resetBox
-chooseXorO
+
+Box "reset"
+assignXorO
+startgame
